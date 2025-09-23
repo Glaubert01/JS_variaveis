2b29d4da9b9f207bfa1740a8c61741383de91149
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 21:05:04 -0300

    update: realizado o exercicio numero 2

--- exercicios.js
+++ exercicios.js
@@ -1,14 +1,21 @@
 //1) Declare três variáveis diferentes (uma para cada tipo: string, número e booleano) e atribua valores a elas. Em seguida, exiba o tipo de cada variável no console.
 
-let nome = "glaubert";
+let texto = "glaubert";
 let bool = true;
 let num = 5;
 
-console.log(typeof nome);
+console.log(typeof texto);
 console.log(typeof bool);
 console.log(typeof num);
 
 //2) Crie duas variáveis, uma contendo seu primeiro nome e outra contendo seu último nome. Em seguida, combine-as em uma terceira variável usando o operador + e em uma quarta variável usando template strings. Por fim, imprima os resultados obtidos no console.
+
+let nome = "Glaubert";
+let sobrenome = " Nunes";
+let concatenado = nome + sobrenome;
+console.log(concatenado);
+let templateString = `${nome}${sobrenome}`;
+console.log(templateString);
 
 //3)Declare duas variáveis, uma contendo um número e outra contendo uma string. Em seguida, combine-as em uma terceira variável usando template strings para montar uma frase e exiba o resultado no console.
 
