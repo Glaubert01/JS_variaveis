272a467f73cc63005ffe04512b65395de89c9679
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 21:09:23 -0300

    update: respondendo o exercicio numero 4

--- exercicios.js
+++ exercicios.js
@@ -26,6 +26,11 @@ console.log(numString);
 
 //4) Crie uma variável inicializada com um valor de qualquer tipo e, em seguida, reatribua um novo valor a essa variável. Imprima ambos os valores no console após atribuí-los.
 
+let teste = 25;
+console.log(teste);
+teste = "Teste";
+console.log(teste);
+
 //5)Declare uma variável usando var fora de um bloco de código (por exemplo, if) e outra dentro desse bloco. Tente acessar essas variáveis dentro do bloco e fora dele utilizando console.log e analise os resultados. Faça o mesmo processo utilizando let e compare com os resultados anteriores.
 
 //6)Declare uma variável booleana que informa se está chovendo e utilize-a em uma estrutura condicional (if) para exibir uma mensagem informando se é preciso levar um guarda-chuva ou não dependendo do valor da variável.
