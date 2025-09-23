ed71b0b2b9a7c6eda12b4fd617f1f944fed0200a
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 21:46:08 -0300

    feat: entendendo sobre tipos booleanos

---
+++ tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = "Fernando";
+const estaAprovado = true;
+
+if (estaAprovado) {
+  console.log("Parabens, boas festas.");
+} else {
+  console.log("Reprovado");
+}
+
+if (estudante === "Fernando") {
+  console.log(`Ola, ${estudante}`);
+} else {
+  console.log("Quem e vc");
+}
