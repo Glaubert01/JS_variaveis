80729faabd686c38bb624002ca5d6b60c31a8caa
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 21:40:34 -0300

    feat: entendendo sobre strings, conversoes e unicode

---
+++ tipo-string.js
@@ -0,0 +1,21 @@
+const estudante = "Caroline";
+const docente = "Ana";
+const cumprimento = "O nosso lembra é 'estudar bastante!'";
+const citacao = `Ju diz: "O nosso lembra é 'estudar bastante!'"`;
+
+console.log(cumprimento);
+console.log(citacao);
+console.log(`A estudante se chama ${estudante}`);
+
+const senha = "SenhaSegura123" + estudante.toUpperCase();
+console.log(senha);
+
+const cifrao = "\u0024";
+const aMaiusculo = "\u0041";
+const tique = "\u2705";
+const hiragana = "\u3041";
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
