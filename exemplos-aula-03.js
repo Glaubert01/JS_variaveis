d3046089b3b0b67aa4ad902981d9962df5ccfb96
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 22:06:37 -0300

    update: exemplos

---
+++ exemplos-aula-03.js
@@ -0,0 +1,15 @@
+Number("1"); // retorna o número 1
+Number("Alura"); // retorna NaN
+Number(undefined); // retorna NaN
+Number(null); // retorna 0
+
+String(2); // retorna '2'
+String(undefined); // retorna 'undefined'
+String(true); // retorna 'true'
+
+"estudando JavaScript".includes("Java"); // retorna true
+
+const texto = "estudando JavaScript";
+texto.includes("Java"); // retorna true
+
+"POR FAVOR, NÃO GRITE".toLowerCase(); // retorna 'por favor, não grite'
