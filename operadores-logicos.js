ef3485108ba0293eaaab94ad42accf9c0d8b7f43
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-22 22:48:00 -0300

    update: operador de negacao

--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,8 +1,15 @@
 const notaFinal = 6;
-const faltas = 5;
+const faltas = 2;
+const advertencias = 0;
 
 if (notaFinal < 7 && faltas > 4) {
   console.log("Reprovado");
 } else {
   console.log("Nao foi reprovado por faltas");
+}
+
+if (faltas >= 2 && !advertencias) {
+  console.log("Recebeu bonus");
+} else {
+  console.log("Nao foi Recebeu bonus");
 }
